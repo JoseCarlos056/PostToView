ee58c73fd7a9e2820acc56efe1a93376cc66bc2a
Jose <josecarlos.depaula17@gmail.com>
2020-03-03 23:05:07 -0300

    adicionado tela de amigos

--- clientside/src/components/Home.js
+++ clientside/src/components/Home.js
@@ -1,77 +1,126 @@
 import React from 'react';
 import logo from './Styles/img/logo.png'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSignOutAlt, faSearch} from '@fortawesome/free-solid-svg-icons';
-import { GlobalStyle, Navbar, Page, Profile, Content } from './Styles/Home'
-export default class Home extends React.Component{
-    constructor(){
+import { faSignOutAlt, faSearch, faUserAltSlash } from '@fortawesome/free-solid-svg-icons';
+import { GlobalStyle, Navbar, Page, Profile, Content, TodoContent, Friends } from './Styles/Home'
+export default class Home extends React.Component {
+    constructor() {
         super();
-        this.state= {
-               
+        this.state = {
+
         }
-	}
+    }
 
-    render(){
-   
-        return(
-        <Page>
-             	<GlobalStyle/>
+    render() {
 
- <Navbar>
-             <div className="logo">
-            <img src={logo} alt="PostToView" />
-             </div>
-             <div className="search">
-                 <form>
-                 <label className="label-input" >
-                 <input type='text' />
-                 <div className="svg">
-                 <FontAwesomeIcon icon={faSearch} />
-                 </div>
-                     </label>
-                 </form>
-             </div>
-             <div className="logout">
-             <FontAwesomeIcon icon={faSignOutAlt} />
-             </div>
-         </Navbar>
-         <Profile>
-            <div className="contentProfile">
-            <div className="circleProfile">
-                
-                </div>
-                <h3>Name of User</h3>
-                <div className="infoProfile">
-                    <ul>
-                        <li>Amigos</li>
-                        <li>Notificações</li>
-                        <li>Convites</li>
-                        <li>Posts</li>
-                    </ul>
-                </div>
+        return (
+            <Page>
+                <GlobalStyle />
+
+                <Navbar>
+                    <div className="logo">
+                        <img src={logo} alt="PostToView" />
+                    </div>
+                    <div className="search">
+                        <form>
+                            <label className="label-input" >
+                                <input type='text' />
+                                <div className="svg">
+                                    <FontAwesomeIcon icon={faSearch} />
+                                </div>
+                            </label>
+                        </form>
+                    </div>
+                    <div className="logout">
+                        <FontAwesomeIcon icon={faSignOutAlt} />
+                    </div>
+                </Navbar>
+                <TodoContent>
+            <Friends>
+            <div className="searchFriend">
+                <label>
+                <input></input>
+                <FontAwesomeIcon icon={faSearch} />
+                </label>
             </div>
-         </Profile>
-         <Content>
-            <div className="post">
-            <div className="profile">
-                    <div className="profileInfo">
-                         <div className="img">
-                        <img></img>
+            <div className="friends">
+            <div className="singlefriends">
+                                    <div className="img">
+                                        <img alt=''></img>
+                                    </div>
+                                    <div className="info">
+                                    <p>Name User</p>
+                                    </div>
+                                    <div className="userSlash">
+                                    <FontAwesomeIcon icon={faUserAltSlash} />
+                                    </div>
+                                    
                 </div>
-                       <p>Name</p>
+                <div className="singlefriends">
+                                    <div className="img">
+                                        <img alt=''></img>
+                                    </div>
+                                    <p>Name User1</p>
                 </div>
+                <div className="singlefriends">
+                                    <div className="img">
+                                        <img alt=''></img>
+                                    </div>
+                                    <p>Name User2</p>
                 </div>
-                <div className="contentPost">
-                    Description
+                <div className="singlefriends">
+                                    <div className="img">
+                                        <img alt=''></img>
+                                    </div>
+                                    <p>Name User3</p>
+                </div>
+                <div className="singlefriends">
+                                    <div className="img">
+                                        <img alt=''></img>
+                                    </div>
+                                    <p>Name User4</p>
+                </div>
+                    </div>
+            </Friends>
+                    <Content>
+                        <div className="post">
+                            <div className="profile">
+                                <div className="profileInfo">
+                                    <div className="img">
+                                        <img alt=''></img>
+                                    </div>
+                                    <p>Name</p>
+                                </div>
+                            </div>
+                            <div className="contentPost">
+                                Description
                     <div className="imgPost">
-                        <img></img>
-                </div>
-                </div>
+                                    <img alt=''></img>
+                                </div>
+                            </div>
 
-            </div>
-         </Content>
-        </Page>
-        
+                        </div>
+                    </Content>
+                    <Profile>
+                        <div className="contentProfile">
+                            <div className="circleProfile">
+
+                            </div>
+                            <h3>Name of User</h3>
+                            <div className="infoProfile">
+                                <ul>
+                                    <li>Amigos</li>
+                                    <li>Notificações</li>
+                                    <li>Convites</li>
+                                    <li>Posts</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </Profile>
+                </TodoContent>
+
+            </Page>
+
         )
     }
 }
--- clientside/src/components/Styles/Home.js
+++ clientside/src/components/Styles/Home.js
@@ -1,4 +1,4 @@
-import styled, { createGlobalStyle, keyframes   } from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css?family=Open+Sans&display=swap');
@@ -10,15 +10,88 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 `
+export const TodoContent = styled.div`
+position: fixed;
+    top: 70px;
+    /* background: blue; */
+    height: 100vh;
+    left: 2px;
+    right: 2px;
+    display: flex;
+    justify-content: space-between;
+    padding: 0px 10px;
+`;
+export const Friends = styled.div`
+position: relative;
+width: 300px;
+background-color: #3E3E87;
+.searchFriend{
+    border-bottom: 1px solid white;
+    margin-bottom: 5px;
+    margin: 0px 5px;
+    label{
+        margin: 5px;
+        border-radius: 5px;
+        background-color: #F2F2F2;
+        height: 40px;
+        display: flex;
+        align-items: center;
+        input{
+            height: 30px;
+            width: 90%;
+            border: none;
+            border-radius: 5px;
+            background-color: #F2F2F2;
+            outline: none;
+            margin: 3px;
+}
+        }
+      
+    }
+}
+.friends{
+    .singlefriends{
+        display: flex;
+        align-items: center;
+        margin: 10px;
+        box-shadow: -1px 5px 8px -4px rgba(0,0,0,0.69);
+        border-radius: 5px;
+        padding: 5px;
+        color: #F2F2F2;
+        font-weight: bold;
+        transition: background-color .3s linear;
+        .img{
+                    height: 40px;
+                    width: 40px;
+                    background: blue;
+                    border-radius: 30px;
+                    margin-right: 15px;
+        }
+        .info{
+            width: 60%;
+        }
+        .userSlash{
+            &:hover{
+                svg{
+                width: 1.65em;
+                height:1.3em;
+        }
+            }
+           
+        }
+        &:hover{
+            background: #3E3EAE;
+        }
+    }
+}
+`;
 export const Page = styled.div`
 position: absolute;
 `;
 export const Profile = styled.div`
-position: fixed;
+position: relative;
 width: 200px;
 height: 100%;
-top: 70px;
-right: 20px;
 .contentProfile{
     text-align: center;
     border-radius: 5px;
@@ -133,13 +206,8 @@ background-color: #6666CC;
 }
 `;
 export const Content = styled.div`
-    margin: auto !important;
-    left: 0 !important;
-    position: fixed;
+    position: relative;
     width: 500px;
-    height: 100%;
-    top: 70px;
-    right: 0;
     background-color: #f2f2f2;
     
     .post{
